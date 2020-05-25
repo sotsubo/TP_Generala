28d4849f737becfcebbd1deede1409a2e7e6c7e0
gastonfranceschini <gastonfranceschini@gmail.com>
2020-05-25 17:53:09 -0300

    route_schema_partidas

---
+++ model/Partida.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const partidaSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require:true,
+            min: 3,
+            max: 255
+        },
+        players: {
+            type: Array,
+            minItems: 2,
+            maxItems: 4,
+            uniqueItems: true,
+            additionalProperties: false,
+            items: {
+                type: Object,
+                required: ["name", "email", "isWinner"],
+                properties: {
+                    name: {
+                        type: String,
+                        require:true,
+                        min: 3,
+                        max: 255
+                    },
+                    email: {
+                        type: String,
+                        required: true,
+                        min: 6,
+                        max:255
+                    },
+                    isWinner: {
+                        type: boolean
+                    }
+                }
+            }
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }
+)
+
+module.exports = mongoose.model('Partida', partidaSchema); 
