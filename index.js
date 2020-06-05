5b422cfd94748867f0996c8bb1e5c0dd19312014
sergio otsubo <sotsubo@gmail.com>
2020-06-05 18:53:36 -0300

    length validadation 2

--- index.js
+++ index.js
@@ -82,7 +82,12 @@ io.on('connection',(socket)=>{
     console.log("connection socket", socket.id);
 
    // io.to(socket.id).emit('refreshSalas', getSalasInLobby());      
- 
+   socket.on('tirardado',( callback) => {
+    console.log('tirardado' );
+    
+    callback(); 
+   }
+   );
     socket.on('joinLobby',({username,lobby}, callback) => {
         // console.log('joinLobby' );
         
@@ -109,7 +114,7 @@ io.on('connection',(socket)=>{
     socket.on('crearSala',({username,sala,lobby,cantMaxUsers}, callback) => {
         console.log('crearSala: ' ,sala )
         const {error, salon} = addSalaLobby({id: socket.id,username, sala,lobby,cantMaxUsers });
-        // socket.join(sala);
+        socket.join(sala);
         console.log("crearSala socket", socket.id);
         if(error) return  callback(error);
         console.log("llamo a getSalas y despues hago un emit refreshSalas");
